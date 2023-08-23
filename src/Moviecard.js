d4fee33c1fcf3ce7b7c497ba2490c2b88ea6ca2d
Sheshant-Manure <msheshant1997@gmail.com>
2023-08-23 17:22:06 +0530

    React state obj

--- src/Moviecard.js
+++ src/Moviecard.js
@@ -1,19 +1,29 @@
 import { Component } from "react";
 
 class MovieCard extends Component {
+    constructor() {
+        super();
+        this.state = {
+            title: "The Avengers",
+            plot: "Supernatural powers shown in the movie",
+            price: 199,
+            rating: 8.9,
+        }
+    }
     render() {
+        const {title, plot, price, rating} = this.state;
         return <div className="main">
             <div className="movie-card">
               <div className="left">
                 <img src="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg" alt="Poster"/>
               </div>
               <div className="right">
-                <div className="title">The Avengers</div>
-                <div className="plot">Supernatural powers shown in the movie.</div>
-                <div className="price">Rs. 199</div>
+                <div className="title">{title}</div>
+                <div className="plot">{plot}</div>
+                <div className="price">{price}</div>
 
                 <div className="footer">
-                    <div className="rating">8.9</div>
+                    <div className="rating">{rating}</div>
                     <div className="star-dis">
                         <img className="str-btn" src="https://cdn-icons-png.flaticon.com/128/43/43625.png" alt="decrease" />
                         <img className="stars" src="https://cdn-icons-png.flaticon.com/128/2107/2107957.png" alt="star" />
