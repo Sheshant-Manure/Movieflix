0f45b65c52d804cb1942df6c8f5ad0ce4b0e1bbf
Sheshant-Manure <msheshant1997@gmail.com>
2023-08-25 00:24:23 +0530

    feat - addStars and removeStars

--- src/Moviecard.js
+++ src/Moviecard.js
@@ -8,10 +8,20 @@ class MovieCard extends Component {
             plot: "Supernatural powers shown in the movie",
             price: 199,
             rating: 8.9,
+            stars: 0.0,
         }
     }
+
+    addStars = () => {
+        this.setState({stars: this.state.stars + 0.5})
+    }
+
+    removeStars = () => {
+        this.setState({stars: this.state.stars - 0.5})
+    }
+
     render() {
-        const {title, plot, price, rating} = this.state;
+        const {title, plot, price, rating, stars} = this.state;
         return <div className="main">
             <div className="movie-card">
               <div className="left">
@@ -25,10 +35,10 @@ class MovieCard extends Component {
                 <div className="footer">
                     <div className="rating">{rating}</div>
                     <div className="star-dis">
-                        <img className="str-btn" src="https://cdn-icons-png.flaticon.com/128/43/43625.png" alt="decrease" />
+                        <img className="str-btn" onClick={this.removeStars} src="https://cdn-icons-png.flaticon.com/128/43/43625.png" alt="decrease" />
                         <img className="stars" src="https://cdn-icons-png.flaticon.com/128/2107/2107957.png" alt="star" />
-                        <img className="str-btn" src="https://cdn-icons-png.flaticon.com/128/748/748113.png" alt="increase" />
-                        <span>0</span>
+                        <img className="str-btn" onClick={this.addStars} src="https://cdn-icons-png.flaticon.com/128/748/748113.png" alt="increase" />
+                        <span>{stars}</span>
                     </div>
                     <button className="favourite-btn">Favourite</button>
                     <button className="cart-btn">Add to Cart</button>
